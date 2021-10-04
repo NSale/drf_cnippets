52cd3cd801402f531ae97ebce37ee8aa6a8e4325
NSale <gospasrb@yahoo.com>
2021-10-04 23:38:10 +0200

    use generics for simplest views

--- snippets/views.py
+++ snippets/views.py
@@ -1,39 +1,20 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
 
 
 # Create your views here.
-class SinppetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SinppetList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SinppetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SinppetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.delete(request, *args, **kwargs)
